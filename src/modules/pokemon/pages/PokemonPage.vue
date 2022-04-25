e7c826f5f90c10be2d48d7e92968eb8d19103c7b
PrintHolamundo <jdohca@gmail.com>
2022-04-24 23:00:08 -0500

    Segmentos del url y QueryParameters

--- src/modules/pokemon/pages/PokemonPage.vue
+++ src/modules/pokemon/pages/PokemonPage.vue
@@ -1,3 +1,24 @@
 <template>
-    <h1>Pokemon Page</h1>
+    <h1>Pokemon<span>#{{ id }}</span></h1>
 </template>
+
+
+<script>
+export default {
+    props: {
+        id: {
+            type: Number,
+            required: true
+        }
+    },
+    data() {
+        return {
+            // pokemon: null
+        }
+    },
+    created() {
+       const {id} = this.$route.params
+         this.id = id
+    },
+}
+</script>
