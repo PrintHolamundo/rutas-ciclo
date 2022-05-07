f103434174abbf18c7e9b84e7bdcde1ea4ace0f8
PrintHolamundo <jdohca@gmail.com>
2022-05-07 13:09:44 -0500

    Peticion HTTP y redirecciones

--- src/modules/pokemon/pages/PokemonPage.vue
+++ src/modules/pokemon/pages/PokemonPage.vue
@@ -1,24 +1,50 @@
 <template>
-    <h1>Pokemon<span>#{{ id }}</span></h1>
+    <h1>
+        Pokemon<span>#{{ id }}</span>
+        <div v-if="pokemon">
+            <img :src="pokemon.sprites.front_default" alt="Pokemon" />
+        </div>
+    </h1>
 </template>
-
 
 <script>
 export default {
     props: {
         id: {
             type: Number,
-            required: true
-        }
+            required: true,
+        },
     },
     data() {
         return {
             // id: this.$route.params.id
-        }
+            pokemon: null,
+        };
     },
     created() {
-    //    const {id} = this.$route.params
-    //      this.id = id
+        //    const {id} = this.$route.params
+        //      this.id = id
+        this.getPokemon();
     },
-}
+    methods: {
+        async getPokemon() {
+            try {
+                const pokemon = await fetch(
+                    `https://pokeapi.co/api/v2/pokemon/${this.id}`
+                ).then((res) => res.json());
+                console.log(pokemon);
+                this.pokemon = pokemon;
+            } catch (error) {
+                this.$router.push('/')
+                console.log('No hay nada que hacer');
+            }
+        },
+    },
+    watch: {
+        id() {
+
+            this.getPokemon();
+        }
+    }
+};
 </script>
--- src/modules/shared/components/Navbar.vue
+++ src/modules/shared/components/Navbar.vue
@@ -1,20 +1,22 @@
 <template>
     <div>
         <router-link to="/">Pokemon List</router-link>
-        <router-link to="/id">Pokemon por id</router-link>
+        <router-link :to="{ name: 'pokemon-id', params: { id: 85 } }">Pokemon por id</router-link>
         <router-link to="/about">About</router-link>
     </div>
 </template>
 
 <style scoped>
-div{
-    padding:0 30px;
+div {
+    padding: 0 30px;
 }
+
 div a {
     font-weight: bold;
     color: #2c3e50;
     margin: 0 10px;
 }
+
 a.router-link-exact-active {
     color: #42b983;
 }
