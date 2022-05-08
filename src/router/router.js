eeef778d5e4cfe53453037de180845af22691aa3
PrintHolamundo <jdohca@gmail.com>
2022-05-08 12:56:27 -0500

    Redireccion desde el router

--- src/router/router.js
+++ src/router/router.js
@@ -6,28 +6,33 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 // import NoPageFound from '../modules/shared/pages/NoPageFound.vue'
 
 
-const routes = [
-    {
-        path: '/',
-        component: () => import('../modules/pokemon/pages/ListPage.vue'),
-    },
-    {
-        path: '/about',
-        component: () => import(/*webpackChunkName: 'AboutPage'*/'@/modules/pokemon/pages/AboutPage')
-    },
-    {
-        path: '/:id',
-        name: 'pokemon-id',
-        component: () => import(/*webpackChunkName: 'PokemonPage'*/'@/modules/pokemon/pages/PokemonPage'),
-        props: (route) => {
-            const id = Number(route.params.id)
-            return isNaN(id) ? { id: 1 } : { id }
-        }
-    },
-    {
-        path: '/:pathMatch(.*)*',
-        component: () => import(/*webpackChunkName: 'NoPageFound'*/'@/modules/shared/pages/NoPageFound')
-    },
+const routes = [{
+    path: '/',
+    redirect: '/home'
+},
+{
+    path: '/home',
+    name: 'home',
+    component: () => import('../modules/pokemon/pages/ListPage.vue'),
+},
+{
+    path: '/about',
+    name: 'about',
+    component: () => import(/*webpackChunkName: 'AboutPage'*/'@/modules/pokemon/pages/AboutPage')
+},
+{
+    path: '/pokemonid/:id',
+    name: 'pokemon-id',
+    component: () => import(/*webpackChunkName: 'PokemonPage'*/'@/modules/pokemon/pages/PokemonPage'),
+    props: (route) => {
+        const id = Number(route.params.id)
+        return isNaN(id) ? { id: 1 } : { id }
+    }
+},
+{
+    path: '/:pathMatch(.*)*',
+    component: () => import(/*webpackChunkName: 'NoPageFound'*/'@/modules/shared/pages/NoPageFound')
+},
 ]
 
 const router = createRouter({
